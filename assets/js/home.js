c11dfbc6ae8ec4111e983bcd95462b71c97f107e
jorge luis camarillo <jlcc170@gmail.com>
2020-01-16 03:34:54 -0600

    ADD upload page, render post and create postPreview

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,10 +6,13 @@ const printPosts = () => {
         method: "GET",
         success: (response) => {
             console.log(response)
+            let maxMiddle = 0;
+            let countPopular = 1;
             postsCollection = response;
             $.each(postsCollection, (i, value) => {
                 if (value.blockDisplay === "list") {
-                    $(".ctn__suggested").append(`
+                    $(".ctn__suggested").find('.extremePostPreview').remove()
+                    $(".ctn__suggested").prepend(`
                 <div class="item__preview d-flex justify-content-between">
                 <div class="left mr-3 d-flex flex-column desc">
                 <span class="category">${value.category}<i class="ml-2">Popular topic</i></span>
@@ -40,8 +43,10 @@ const printPosts = () => {
                                         </div>
                 </div>
                 `)
-                } if (value.blockDisplay === "featured") {
-                    $("#featured-post").append(`
+                }
+                if (value.blockDisplay === "featured") {
+                    $("#featured-post").find('.extremePostPreview').remove()
+                    $("#featured-post").html(`
                     <div class=" d-flex justify-content-between  flex-column item__preview hero__item hero__left">
                     <div class="item__image d-flex img__post">
                         <img src='${value.imgDataLarge}'
@@ -49,16 +54,14 @@ const printPosts = () => {
                     </div>
                     <div class="left d-flex flex-column desc mt-2">
                         <h2 class="title__item">${value.title}</h2>
-                        <p class="description">Use this promo code to land yourself a new lady. (ME!) Exclusions
-                            apply.</p>
+                        <p class="description">${value.content}</p>
                         <div class="d-flex justify-content-between">
                             <div class=" mt-3 mr-2 ellipsis">
-                                <p class="ellipsis"><span class="autor">Javascript Jeep🚙💨</span> in <span
-                                        class="group">Better
-                                        Programming</span></p>
+                                <p class="ellipsis"><span class="autor">${value.author}</span> in <span
+                                        class="group">${value.category}</span></p>
                                 <p class="ellipsis">
-                                    <span class="timeago">a few hours ago</span> .
-                                    <span class="read">2 min read</span>
+                                    <span class="timeago">${value.date}</span> .
+                                    <span class="read">${value.minute} minutes to read</span>
                                     <span class="icon-star1 "></span>
                                 </p>
 
@@ -69,10 +72,131 @@ const printPosts = () => {
                 </div>
                 `)
                 }
+                if (value.blockDisplay === "highlight") {
+                    $("#highlight__post").find('.extremePostPreview').remove()
+                    $("#highlight__post").html(`
+                            <div class=" d-flex justify-content-between  flex-column item__preview hero__item hero__right">
+                            <div class="item__image d-flex img__post">
+                                <img src="${value.imgDataLarge}" alt="">
+                            </div>
+                            <div class="left mr d-flex flex-column desc">
+                                <h2 class="title__item">${value.title}</h2>
+                                <span class="description">${value.content}</span>
+                                <div class="d-flex justify-content-between">
+                                    <div class=" mt-3 mr-2 ellipsis">
+                                        <p class="ellipsis"><span class="autor">${value.author}</span> in <span class="group">${value.category}</span></p>
+                                        <p class="ellipsis">
+                                            <span class="timeago">${value.date}</span> .
+                                            <span class="read">${value.minute} min read</span>
+                                            <span class="icon-star1 "></span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `)
+                }
+                if (value.blockDisplay === "middle") {
+                    // clean preload image
+                    $("#middle__post").find('.extremePostPreview').remove()
+                    maxMiddle ++;
+                    console.log(i, value)
+                    if(maxMiddle <= 3){
+                        $("#middle__post").prepend(`
+                        <div class=" d-flex justify-content-between  flex-row-reverse flex-md-row item__preview small">
+                            <div class="item__image d-flex img__post">
+                                <img src="${value.imgDataSquare}" alt="">
+                            </div>
+                            <div class="left ml-md-3 d-flex flex-column desc">
+                                <h2 class="title__item">${value.title}</h2>
+                                <span class="description d-block d-md-none">${value.content}</span>
+                                <div class="d-flex justify-content-between">
+                                    <div class=" mt-1 mr-2 ellipsis">
+                                        <p class="ellipsis"><span class="autor">${value.author}</span> in <span class="group">${value.category}</span></p>
+                                        <p class="ellipsis">
+                                            <span class="timeago">${value.date}</span> .
+                                            <span class="read">${value.minute} min read</span>
+                                            <span class="icon-star1 "></span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        `)
+                    }
+                }
+                if (value.blockDisplay === "popular") {
+                    $("#popular__post").find('.extremePostPreview').remove()
+                    $("#popular__post").append(`
+                    <div class="item__preview d-flex justify-content-between">
+                        <div class="left mr-3 d-flex flex-column">
+                            <h4 class="category_number">${countPopular}</h4>
+                        </div>
+                        <div class="left mr-3 d-flex flex-column">
+                            <h2 class="title__item small">${value.title}</h2>
+                            <div class=" mt-3 mr-2 ellipsis">
+                                <p class="ellipsis">
+                                    <span class="autor">${value.author}</span> in <span class="group">${value.category}</span>
+                                </p>
+                                <p class="ellipsis">
+                                    <span class="timeago">${value.date}</span> .
+                                    <span class="read">${value.minute} min read</span>
+                                    <span class="icon-star1 "></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+                    countPopular++
+                }
+                if (value.blockDisplay === "network") {
+                    $("#network__post").find('.extremePostPreview').remove()
+                    $("#network__post").prepend(`
+                    <div class="item__preview d-flex justify-content-between mt-3 mb-2 px-2 py-2">
+                            <div class="left mr-3 d-flex flex-column">
+                                <span class="active__img"><img src="${value.imgDataNetwork}" alt="" class="medium__avatar"></span>
+                            </div>
+                            <div class="left mr-1 d-flex flex-column">
+                                <h2 class="title__item small">${value.title}</h2>
+                                <div class=" mt-3 mr-2 ellipsis">
+                                    <p class="ellipsis">
+                                        <span class="autor">${value.author}</span> in <span class="group">${value.category}</span>
+                                    </p>
+                                    <p class="ellipsis">
+                                        <span class="timeago">${value.date}</span> .
+                                        <span class="read">${value.minute} min read</span>
+                                        <span class="icon-star1 "></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    `)
+                }
+                if (value.blockDisplay === "reading") {
+                    $("#reading__post").find('.extremePostPreview').remove()
+                    $("#reading__post").prepend(`
+                        <div class="item__preview d-flex justify-content-between mt-3 mb-2 px-2 py-2">
+                            <div class="left mr-3 d-flex flex-column">
+                                <span class="icon__book"><span class="icon-bookmark "></span></span>
+                            </div>
+                            <div class="left mr-3 d-flex flex-column">
+                                <h2 class="title__item small">${value.title}</h2>
+                                <div class=" mt-1 mr-2 ellipsis">
+                                    <p class="ellipsis">
+                                        <span class="autor">${value.author}</span> in <span class="group">${value.category}</span>
+                                    </p>
+                                    <p class="ellipsis">
+                                        <span class="timeago">${value.date}</span> .
+                                        <span class="read">${value.minute} min read</span>
+                                        <span class="icon-star1 "></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    `)
+                }
             })
         }
     })
 }
 printPosts()
-
-
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,3 +1,92 @@
-$(function(){
+function encodeImageFileAsURL(element) {
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function () {
+    console.log('RESULT', reader.result)
+    $(element).next('input').val(reader.result)
+  }
+  reader.readAsDataURL(file);
+}
+$(function () {
+  console.log('ready')
+
+  $('.send__post').click(function (e) {
+    e.preventDefault()
+    
+    // get values
+    let title = $('input[name="title__post"]').val();
+    let content = $('textarea[name="content__post"]').val();
+    let date = getDate();
+    let imgDataSquare = $('input[name="img__large"]').val();
+    let imgDataLarge = $('input[name="img__square"]').val();
+    let minute = $('input[name="minutes"]').val();
+    let category = $('select[name="category__post"]').val();
+    let author = $('input[name="author__post"]').val();
+    let group = $('input[name="group__post"]').val();
+    let blockDisplay = $('select[name="blockDisplay__post"]').val();
+    let imgDataNetwork = $('input[name="img__network"]').val();
+
+
+    var objectPost = {
+      title,
+      content,
+      date,
+      imgDataSquare,
+      imgDataLarge,
+      minute,
+      category,
+      author,
+      group,
+      blockDisplay,
+      imgDataNetwork
+    }
+    uploadPost(objectPost)
+    
+  })
+
+  const uploadPost = (userData) => {
+    $.ajax({
+      url: "https://blog-6g.firebaseio.com/team1.json",
+      method: "POST",
+      data: JSON.stringify(userData),
+      success: (response) => {
+        console.log(response);
+        //printUsers();
+      }
+    });
+  }
+
+  // var dataUpload = {
+  //   title: "some title",
+  //   content: "some content",
+  //   date: "some date",
+  //   imgDataSquare: "some url",
+  //   imgDataLarge: "some url",
+  //   minute: "2",
+  //   category: "Better Programming",
+  //   author: "Tim Dimming",
+  //   group: "The Writing Cooperative",
+  //   blockDisplay: 'list',
+  //   //blockDisplay: list, 'featured', 'Highlight', 'Network', 'Popular', 'reading',
+  // }
+
+  $('#select__display').change(function(){
+    if($('#select__display') === "network"){
+      $('#ctn__img__network').removeClass('d-none')
+    }else{
+      $('#ctn__img__network').addClass('d-none')
+    }
+  })
+
   
 })
+
+
+function getDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = yyyy + '/' + mm + '/' + dd;
+  return today
+}
